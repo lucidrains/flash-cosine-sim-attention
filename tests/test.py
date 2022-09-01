d73354140504764d5b4d7f0517a3b83c9087f795
Phil Wang <lucidrains@gmail.com>
2022-09-01 15:47:40 -0700

    pytest

--- tests/test.py
+++ tests/test.py
@@ -1,35 +1,40 @@
 import torch
+import pytest
 from flash_cosine_sim_attention import plain_cosine_sim_attention, flash_cosine_sim_attention
+
+assert torch.cuda.is_available(), 'cuda must be available'
 
 # helper functions
 
-def allclose(a, b, atol = 1e-6):
+def allclose(a, b, atol = 1e-5):
     diff = (a - b).abs().amax()
     return diff <= atol
 
 # tests
 
-def test_output_equal():
+@pytest.mark.parametrize('causal', [True, False])
+def test_output_equal(causal):
     q = torch.randn(2, 4, 63, 63).cuda()
     k = torch.randn(2, 4, 63, 63).cuda()
     v = torch.randn(2, 4, 63, 63).cuda()
-    plain_output = plain_cosine_sim_attention(q, k, v)
-    flash_output = flash_cosine_sim_attention(q, k, v)
+    plain_output = plain_cosine_sim_attention(q, k, v, causal = causal)
+    flash_output = flash_cosine_sim_attention(q, k, v, causal = causal)
     assert allclose(plain_output, flash_output)
 
-def test_grad_equal():
+@pytest.mark.parametrize('causal', [True, False])
+def test_grad_equal(causal):
     q = torch.randn(2, 4, 63, 63).cuda().requires_grad_()
     k = torch.randn(2, 4, 63, 63).cuda().requires_grad_()
     v = torch.randn(2, 4, 63, 63).cuda().requires_grad_()
 
-    plain_output = plain_cosine_sim_attention(q, k, v)
+    plain_output = plain_cosine_sim_attention(q, k, v, causal = causal)
     plain_output.sum().backward()
 
     dq, dk, dv = q.grad, k.grad, v.grad
 
     q.grad, k.grad, v.grad = None, None, None
 
-    flash_output = flash_cosine_sim_attention(q, k, v)
+    flash_output = flash_cosine_sim_attention(q, k, v, causal = causal)
     flash_output.sum().backward()
 
     fdq, fdk, fdv = q.grad, k.grad, v.grad
